4c57f7baf824831cb976576a5333938c3cdfd6f8
Alexandr Baranov <baranovau216@gmail.com>
2022-11-28 10:22:58 +0700

    update

--- 2.lisp
+++ 2.lisp
@@ -5,3 +5,8 @@
 						  (expand-list (car l) result)))))
 
 (expand-list `((1 2) () 3 (4 5 NIL) 15 ((16)) (18 (17))))
+
+(defun expand-list (l)
+	(cond ((null l) NIL)
+		  ((atom (car l)) (cons (car l) (expand-list (cdr l))))
+		  (T (append (expand-list (car l)) (expand-list (cdr l))))))
