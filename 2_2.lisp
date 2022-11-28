1429d9a15d9bc0e63aa3ce1e1b645dc36ba1e9ae
Alexandr Baranov <baranovau216@gmail.com>
2022-11-28 10:17:32 +0700

    fix to O(n)

--- 2_2.lisp
+++ 2_2.lisp
@@ -7,3 +7,16 @@
                        (find-all (car l) to-find result (append way (list pos-in-cur-list)) 1)
                        way
                        (+ 1 pos-in-cur-list)))))
+
+;; todo: fix (car r)
+(defun find-all (l to-find &optional (pos-in-cur-list 1))
+    (cond ((null l) NIL)
+          ((equal (car l) to-find) (cons (list pos-in-cur-list) (find-all (cdr l) to-find (+ 1 pos-in-cur-list))))
+          ((atom (car l)) (find-all (cdr l) to-find (+ 1 pos-in-cur-list)))
+          ((not (null (setq r (find-all (car l) to-find)))) (cons (cons pos-in-cur-list (car r)) 
+                                                                  (find-all (cdr l) to-find (+ 1 pos-in-cur-list))))
+          (T (find-all (cdr l) to-find (+ 1 pos-in-cur-list)))))
+
+(print (find-all `(1 (3 1 2) ((1 2) 3 (1 2)) (2 (1 2 3)3 (0 (1 6))) 4 1) `(1 2)))
+(print (find-all `(1 (2 1) 3 (2 3 (0 (1 6))) 4 1) `1))
+(print (find-all `(2 1) 1))
--- 2_2_B.lisp
+++ 2_2_B.lisp
@@ -1,6 +1,8 @@
-(defun find-first (l to-find &optional way (pos-in-cur-list 1))
+(defun find-first (l to-find &optional (pos-in-cur-list 1))
     (cond ((null l) NIL)
-          ((equal (car l) to-find) (append way (list pos-in-cur-list)))
-          ((atom (car l)) (find-first (cdr l) to-find way (+ 1 pos-in-cur-list)))
-          ((not (null (setq r (find-first (car l) to-find (append way (list pos-in-cur-list)) 1)))) r)
-          (T (find-first (cdr l) to-find way (+ 1 pos-in-cur-list)))))
+          ((equal (car l) to-find) (list pos-in-cur-list))
+          ((atom (car l)) (find-first (cdr l) to-find (+ 1 pos-in-cur-list)))
+          ((not (null (setq r (find-first (car l) to-find)))) (cons pos-in-cur-list r))
+          (T (find-first (cdr l) to-find (+ 1 pos-in-cur-list)))))
+
+(print (find-first `((7 7 (0) 8 NIL) 12 3 (1 (2 ((7 (0) 8 NIL) 8) 45))) `(7 (0) 8 NIL)))
